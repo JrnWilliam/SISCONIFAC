2a60d5b23411a8490ea838b8e515526daff6bcbc
JrnWilliam <antoni.will91@gmail.com>
2024-06-06 15:13:32 -0600

    Se creo el archivo config.php que contiene las constantes de la configuracion a la BD

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+    define('DBHOST', 'localhost:3307');
+    define('DB_NAME', "sistema");
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_ENCODE', 'utf8');
+    define('PRO_NOMBRE', 'SISCONIFAC');
+?>
