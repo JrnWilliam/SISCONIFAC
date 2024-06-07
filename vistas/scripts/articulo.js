9177cb9b141da0760039e23b3a1f2a7c3c8b7152
JrnWilliam <antoni.will91@gmail.com>
2024-06-06 19:31:10 -0600

    Se creo dentro de la carpeta scripts el archivo articulo.js en el cual se encuentran las funciones para darle dinamismo a la pagina de articulo

---
+++ vistas/scripts/articulo.js
@@ -0,0 +1,76 @@
+var tablaarticulo
+
+function IniciarArticulos()
+{
+    MostrarFormulario(false)
+}
+
+//Funcion que se encargara de limpiar los campos
+function LimpiarCampos()
+{
+    $("#codigo").val("")
+    $("#nombre").val("")
+    $("#descripcion").val("")
+    $("#stock").val("")
+}
+//Funcion que nos mostrara el formulario al momento que vallamos a ingresar un articulo o se requiera editar
+function MostrarFormulario(valor)
+{
+    LimpiarCampos()
+    if (valor)
+    {
+        $("#listado_registros").hide()
+        $("#formulario_registros").show()
+        $("#BtnGuardar").prop("disabled",false)
+        $("BtnAgregar").hide()
+    }
+    else
+    {
+        $("#listado_registros").show()
+        $("#formulario_registros").hide()
+        $("#BtnAgregar").show()
+    }
+}
+
+//Funcion que nos permitira ocultar o cerrar el formulario
+function CerrarFormulario()
+{
+    LimpiarCampos()
+    MostrarFormulario(false)
+}
+
+//Función que mostrara en lista los valores guardados en la tabla articulos
+function ListarRegistros()
+{
+    tablaarticulo = $('#TablaListadoArticulos').dataTable(
+        {
+            "aProcessing": true,
+            "aServerSide": true,
+            dom: 'Bfrtip',
+            buttons: [
+                'copyHtml5',
+                'excelHtml5',
+                'csvHtml5',
+                'pdf'
+            ],
+            "ajax":
+            {
+                url: '../ajax/Articulo.php?operacion=Listar',
+                type: "get",
+                dataType: "json",
+                error: function(e)
+                {
+                    console.log(e.responseText);
+                }
+            },
+            "bDestroy": true,
+            "iDisplayLength": 10,
+            "order": [[0, "asc"]]
+        }
+    ).DataTable()
+}
+
+//Funcion encargada de guardar los articulos que se agreguen a la base de datos
+
+
+IniciarArticulos()
