16026fee42ace6b50d71fbd7d400f88b7774d152
JrnWilliam <antoni.will91@gmail.com>
2024-06-22 12:22:19 -0600

    Se creo la funcion ListarPermisos

--- vistas/scripts/permiso.js
+++ vistas/scripts/permiso.js
@@ -3,6 +3,7 @@ var tablapermiso
 function IniciarPermisos()
 {
     MostrarFormularioPermiso(false)
+    ListarPermisos()
 }
 
 function MostrarFormularioPermiso(valor)
@@ -21,3 +22,33 @@ function MostrarFormularioPermiso(valor)
         $("#BtnAgregar").hide()
     }
 }
+
+function ListarPermisos()
+{
+    tablapermiso = $("#TablaListadoPermisos").dataTable({
+        "aProcessing": true,
+        "aServerSide": true,
+        dom: 'Bfrtip',
+        buttons: [
+            'copyHtml5',
+            'excelHtml5',
+            'csvHtml5',
+            'pdf'
+        ],
+        "ajax":
+        {
+            url: "../ajax/Permiso.php?operacion=MostrarPermisos",
+            type: "get",
+            dataType: "json",
+            error: function(e)
+            {
+                console.log(e.responseText)
+            }
+        },
+        "bDestroy": true,
+        "iDisplayLenght":10,
+        "order": [[0,"asc"]]
+    }).dataTable()
+}
+
+IniciarPermisos()
