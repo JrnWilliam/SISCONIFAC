8bb1706892264857602a0e9c8099e50cf8337821
JrnWilliam <antoni.will91@gmail.com>
2024-06-22 12:23:35 -0600

    Se creo la tabla para listar permisos y se establecion la conexion con esta

---
+++ vistas/VPermiso.php
@@ -0,0 +1,45 @@
+<?php
+  require "VHeader.php"  
+?>
+<div class="content-wrapper">
+  <section class="content">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="box">
+          <div class="box-header with-border">
+            <h1 class="box-title">
+              Listado de Permisos del Sistema
+              <button class="btn btn-success" onclick="MostrarFormulario(true)" id="BtnAgregar">
+                <i class="fa fa-plus-circle"></i> Agregar
+              </button>
+            </h1>
+            <div class="box-tools pull-right"></div>
+          </div>
+          <!-- /.box-header -->
+          <!-- centro -->
+          <div class="panel-body table-responsive" id="TablaPermisos">
+            <table id="TablaListadoPermisos" class="table table-striped table-bordered table-condensed table-hover">
+              <thead>
+                <th>Nombre</th>
+              </thead>
+              <tbody>
+              </tbody>
+              <tfoot>
+                <th>Nombre</th>
+              </tfoot>
+            </table>
+          </div>
+        </div>
+        <!-- /.box -->
+      </div>
+      <!-- /.col -->
+    </div>
+    <!-- /.row -->
+  </section>
+  <!-- /.content -->
+</div>
+<?php
+require "VFooter.php"  
+?>
+
+<script src="../vistas/scripts/permiso.js"></script>
