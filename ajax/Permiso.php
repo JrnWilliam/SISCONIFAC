57737bf7241fd7efe8a0d3ce73166eed2467056c
JrnWilliam <antoni.will91@gmail.com>
2024-06-21 21:44:18 -0600

    Se creo el case MostrarPermisos para pasarle la petiocion relacionada a mostrar los permisos en el datatable

--- ajax/Permiso.php
+++ ajax/Permiso.php
@@ -1,3 +1,28 @@
 <?php
-require_once "../modelos/CPermiso.php"  
+require_once "../modelos/CPermiso.php";
+
+$permiso = new CPermiso();
+
+switch($_GET["operacion"])
+{
+    case 'MostrarPermisos':
+        $respuesta = $permiso->MostrarPermisos();
+        $data = Array();
+
+        while($registro = $respuesta->fetch_object())
+        {
+            $data = array(
+                "0" => $registro->nombre
+            );
+        }
+        $resultado = array(
+            "sEcho"=>1,
+            "iTotalRecords"=>count($data),
+            "iTotalDisplayRecords"=>count($data),
+            "aaData"=>$data
+        );
+        echo json_encode($resultado);
+    break;
+}
+
 ?>
