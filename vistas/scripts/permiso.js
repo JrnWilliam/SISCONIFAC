b81ef8878f56b4df7d318196e51c9d3329c7b6bc
JrnWilliam <antoni.will91@gmail.com>
2024-06-21 23:17:06 -0600

    Se agrego la funcion MostrarFormularioPermiso

--- vistas/scripts/permiso.js
+++ vistas/scripts/permiso.js
@@ -2,5 +2,22 @@ var tablapermiso
 
 function IniciarPermisos()
 {
-    
+    MostrarFormularioPermiso(false)
+}
+
+function MostrarFormularioPermiso(valor)
+{
+    if(valor)
+    {
+        $("#TablaPermiso").hide()
+        $("#FormularioPermiso").show()
+        $("#BtnGuardar").prop("disabled",false)
+        $("#BtnAgregar").hide()
+    }
+    else
+    {
+        $("#TablaPermiso").hide()
+        $("#FormularioPermiso").show()
+        $("#BtnAgregar").hide()
+    }
 }
