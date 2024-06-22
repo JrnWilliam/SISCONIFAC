9e92344341c7d9e8e33e0df4cd1d212bcb3d2902
JrnWilliam <antoni.will91@gmail.com>
2024-06-21 21:17:25 -0600

    Se creo el constructor y metodo para listar los permisos

--- modelos/CPermiso.php
+++ modelos/CPermiso.php
@@ -1,3 +1,17 @@
 <?php
-require "../config/conexion.php";  
+require "../config/conexion.php";
+
+class CPermiso
+{
+    public function __construct()
+    {
+        
+    }
+
+    public function ListarPermisos()
+    {
+        $sql = "SELECT *FROM permiso";
+        return Ejecutar_Consulta($sql);
+    }
+}
 ?>
