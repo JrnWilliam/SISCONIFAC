087ada8947d06da352737d5c8854eaf04eef4e20
JrnWilliam <antoni.will91@gmail.com>
2024-06-25 09:27:04 -0600

    Se implementaron los metodos con el fin de inicializar y limpiar las variables que se recibiran a taves del metodo post

--- ajax/Usuario.php
+++ ajax/Usuario.php
@@ -2,4 +2,19 @@
 require_once "../modelos/CUsuario.php";
 
 $usuario = new Cusuario();
+
+$idusuario = isset($_POST["idusuario"])?LimpiarCadena($_POST["idusuario"]):"";
+$nombre = isset($_POST["nombre"])?LimpiarCadena($_POST["nombre"]):"";
+$tipodocumento = isset($_POST["tipodocumento"])?LimpiarCadena($_POST["tipodocumento"]):"";
+$numdocumento = isset($_POST["numdocumento"])?LimpiarCadena($_POST["numdocumento"]):"";
+$direccion = isset($_POST["direccion"])?LimpiarCadena($_POST["direccion"]):"";
+$telefono = isset($_POST["telefono"])?LimpiarCadena($_POST["telefono"]):"";
+$email = isset($_POST["email"])?LimpiarCadena($_POST["email"]):"";
+$cargo = isset($_POST["cargo"])?LimpiarCadena($_POST["cargo"]):"";
+$login = isset($_POST["login"])?LimpiarCadena($_POST["login"]):"";
+$clave = isset($_POST["clave"])?LimpiarCadena($_POST["clave"]):"";
+$imagen = isset($_POST["imagen"])?LimpiarCadena($_POST["imagen"]):"";
+$condicion = isset($_POST["condicion"])?LimpiarCadena($_POST["condicion"]):"";
+
+
 ?>
