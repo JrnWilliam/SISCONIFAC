e56f161a00329e1c41809c599a447f397e851141
JrnWilliam <antoni.will91@gmail.com>
2024-06-26 12:22:55 -0600

    Se creo la funcion LimpiarCamposUsuarios

--- vistas/scripts/usuario.js
+++ vistas/scripts/usuario.js
@@ -5,9 +5,25 @@ function IniciarUsuarios()
 
 }
 
+function LimpiarCamposUsuarios()
+{
+    $("#idusuario").val("")
+    $("#nombre").val("")
+    $("#tipodocumento").val("").selectpicker("refresh")
+    $("#numdocumento").val("")
+    $("#direccion").val("")
+    $("#telefono").val("")
+    $("#email").val("")
+    // $
+    // $
+    // $
+    // $
+    // $
+}
+
 function MostrarFormularioUsuario(valor)
 {
-    
+    LimpiarCamposUsuarios()
 }
 
 IniciarUsuarios()
