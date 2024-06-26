b10e2d30ca99c1330fce6946a54101a2906d61b4
JrnWilliam <antoni.will91@gmail.com>
2024-06-26 12:15:43 -0600

    Se agrego la referencia a la vista VUsuario.php

--- vistas/VHeader.php
+++ vistas/VHeader.php
@@ -123,7 +123,7 @@
                 <i class="fa fa-angle-left pull-right"></i>
               </a>
               <ul class="treeview-menu">
-                <li><a href="usuario.php"><i class="fa fa-circle-o"></i> Usuarios</a></li>
+                <li><a href="VUsuario.php"><i class="fa fa-circle-o"></i> Usuarios</a></li>
                 <li><a href="VPermiso.php"><i class="fa fa-circle-o"></i> Permisos</a></li>
                 
               </ul>
