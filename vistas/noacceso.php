8199eb899f41090590a9df9bda6fbadd3452ff84
JrnWilliam <antoni.will91@gmail.com>
2024-07-04 18:44:08 -0600

    Se creo la vista que se mostrara en caso un usuario no tenga acceso a alguna funcionalidad del sistema

---
+++ vistas/noacceso.php
@@ -0,0 +1,30 @@
+<div class="content-wrapper">
+  <section class="content">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="box">
+          <div class="box-header with-border">
+            <h1 class="box-title">
+              No Tiene Acceso a Esta Pagina
+            </h1>
+            <div class="box-tools pull-right"></div>
+          </div>
+          <!-- /.box-header -->
+          <!-- centro -->
+           <div class="centronoacceso">
+           <h1 class="h1noacceso">
+            No Tienes Acceso a Esta Funcionalidad
+           </h1>
+           <p class="pnoacceso">
+            Comuniquese con el Administrador
+           </p>
+           </div>
+        </div>
+        <!-- /.box -->
+      </div>
+      <!-- /.col -->
+    </div>
+    <!-- /.row -->
+  </section>
+  <!-- /.content -->
+</div>
