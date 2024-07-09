dad0e68f76fb9be8a72ab73982a0b56d42e384fa
JrnWilliam <antoni.will91@gmail.com>
2024-07-09 17:47:59 -0600

    Se creo el metodo para poder Registrar un ingreso

---
+++ modelos/CIngresos.php
@@ -0,0 +1,28 @@
+<?php
+require "../config/conexion.php";
+
+Class CIngresos
+{
+    public function __construct()
+    {
+
+    }
+
+    public function RegistrarIngreso($idproveedor,$idusuario,$tipocomprobante,$seriecomprobante,$numcomprobante,$fechahora,$impuesto,$totalcompra,$idarticulo,$cantidad,$preciocompra,$precioventa)
+    {
+        $sql = "INSERT INTO ingreso (idproveedor,idusuario,tipo_comprobante,serie_comprobante,num_comprobante,fecha_hora,impuesto,total_compra,estado) VALUES ('$idproveedor','$idusuario','$tipocomprobante','$seriecomprobante','$numcomprobante','$fechahora','$impuesto','$totalcompra','Aceptado')";
+
+        $idnuevoingreso = EjecutarConsultaRetornarID($sql);
+
+        $numingresos = 0;
+        $centinela = true;
+
+        while($numingresos < count($idarticulo))
+        {
+            $consulta = "INSERT INTO detalle_ingreso (idingreso,idarticulo,cantidad,precio_compra,precio_venta) VALUES ('$idnuevoingreso','$idarticulo[$numingresos]','$cantidad[$numingresos]','$preciocompra[$numingresos]','$precioventa[$numingresos]')";
+            Ejecutar_Consulta($consulta) || $centinela = false;
+            $numingresos+=1;
+        }
+    }
+}
+?>
