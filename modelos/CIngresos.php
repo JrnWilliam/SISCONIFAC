8eb3fe745e1d674b7520c404c67278e5ff75af56
JrnWilliam <antoni.will91@gmail.com>
2024-07-14 21:04:58 -0600

    Se agrego el metodo que nos mostrara los detalles de los ingresos registrados

--- modelos/CIngresos.php
+++ modelos/CIngresos.php
@@ -38,6 +38,12 @@ Class CIngresos
         return EjecutarConsultaSimpleFila($sql);
     }
 
+    public function SeleccionarRegistroDetalleIngreso($idingreso)
+    {
+        $sql = "SELECT di.idingreso,di.idarticulo,a.nombre,di.cantidad,di.precio_compra,di.precio_venta FROM detalle_ingreso di INNER JOIN articulo a on di.idarticulo=a.idarticulo WHERE di.idingreso='$idingreso'";
+        return Ejecutar_Consulta($sql);
+    }
+
     public function MostrarIngresos()
     {
         $sql = "SELECT i.idingreso, DATE(i.fecha_hora) AS fecha,i.idproveedor,p.nombre AS proveedor,u.idusuario,u.nombre AS usuario,i.tipo_comprobante,i.serie_comprobante,i.num_comprobante,i.total_compra,i.impuesto,i.estado FROM ingreso i INNER JOIN  persona p ON i.idproveedor=p.idpersona INNER JOIN usuario u ON i.idusuario=u.idusuario";
