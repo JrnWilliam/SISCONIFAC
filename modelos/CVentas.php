862a325c90878e751238642551ff675dd263cd03
JrnWilliam <antoni.will91@gmail.com>
2024-07-18 19:47:40 -0600

    Se creo el metodo para insertar una nueva venta junto con los detalles de esta

--- modelos/CVentas.php
+++ modelos/CVentas.php
@@ -8,6 +8,21 @@ Class CVentas
         
     }
 
-    
+    public function InsertarVentas($idcliente,$idusuario,$tipocomprobante,$seriecomprobante,$numcomprobante,$fechahora,$impuesto,$totalventa,$idarticulo,$cantidad,$precioventa,$descuento)
+    {
+        $sql = "INSERT INTO venta(idcliente, idusuario, tipo_comprobante, serie_comprobante, num_comprobante, fecha_hora, impuesto, total_venta, estado) VALUES ('$idcliente','$idusuario','$tipocomprobante','$seriecomprobante','$numcomprobante','$fechahora','$impuesto','$totalventa','Aceptado')";
+        $idventanueva = EjecutarConsultaRetornarID($sql);
+
+        $numventas = 0;
+        $centinela = true;
+
+        while($numventas < count($idarticulo))
+        {
+            $consulta = "INSERT INTO detalle_venta(idventa,idarticulo,cantidad,precio_venta,descuento) VALUES ('$idventanueva','$idarticulo[$numventas]','$cantidad[$numventas]','$precioventa[$numventas]','$descuento[$numventas]')";
+            Ejecutar_Consulta($consulta) or $centinela = false;
+            $numventas+=1;
+        }
+        return $centinela;
+    }
 }
 ?>
