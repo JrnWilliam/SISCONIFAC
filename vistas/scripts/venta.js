71b51a0061073c57008843342995939218686950
JrnWilliam <antoni.will91@gmail.com>
2024-07-21 12:02:37 -0600

    Se creo la funcion MostrarFormularioVenta y LimpiarCampos

--- vistas/scripts/venta.js
+++ vistas/scripts/venta.js
@@ -5,4 +5,31 @@ function InicarVenta()
 
 }
 
+function MostrarFormularioVenta(valor)
+{
+    LimpiarCampos()
+    if(valor)
+    {
+        $("#TablaVenta").hide()
+        $("#FormularioVenta").show()
+        $("#BtnAgregar").hide()
+    }
+    else
+    {
+        $("#TableVenta").show()
+        $("#FormularioVenta").hide()
+        $("#BtnAgregar").show()
+    }
+}
+
+function LimpiarCampos()
+{
+    $("#idventa").val("")
+    $("#idcliente").val('').selectpicker('refresh')
+    $("#tipocomprobante").val('').selectpicker('refresh')
+    $("#seriecomprobante").val("")
+    $("#numcomprobante").val("")
+    $("#impuesto").val("")
+}
+
 InicarVenta()
