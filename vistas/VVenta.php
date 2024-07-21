76470d2f6bb7a8ec9136316b377b5e923bd376dc
JrnWilliam <antoni.will91@gmail.com>
2024-07-21 17:49:34 -0600

    Se agrego el codigo con variables de sesion y funciones encargadas de controlar la sesion y el acceso de los usuarios registrados

--- vistas/VVenta.php
+++ vistas/VVenta.php
@@ -1,3 +1,28 @@
 <?php
-    
+ob_start();
+session_start();
+
+if(!isset($_SESSION["nombre"]))
+{
+    header("Location: login.html");
+}
+else
+{
+    require "VHeader.php";
+    if($_SESSION['ventas'] == 1)
+    {
+?>
+
+<?php
+    }
+    else
+    {
+        require "noacceso.php";
+    }
+    require "VFooter.php";
+?>
+<script src="../vistas/scripts/venta.js"></script>
+<?php
+}
+ob_end_flush(); 
 ?>
