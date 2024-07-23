cdc35488eb8acc17e6bc85f3447992ee87f86f5b
JrnWilliam <antoni.will91@gmail.com>
2024-07-23 12:24:21 -0600

    Se corrigio en algunas consultas el nombre de la tabla

--- modelos/CVentas.php
+++ modelos/CVentas.php
@@ -10,7 +10,7 @@ Class CVentas
 
     public function InsertarVentas($idcliente,$idusuario,$tipocomprobante,$seriecomprobante,$numcomprobante,$fechahora,$impuesto,$totalventa,$idarticulo,$cantidad,$precioventa,$descuento)
     {
-        $sql = "INSERT INTO venta(idcliente, idusuario, tipo_comprobante, serie_comprobante, num_comprobante, fecha_hora, impuesto, total_venta, estado) VALUES ('$idcliente','$idusuario','$tipocomprobante','$seriecomprobante','$numcomprobante','$fechahora','$impuesto','$totalventa','Aceptado')";
+        $sql = "INSERT INTO ventas(idcliente, idusuario, tipo_comprobante, serie_comprobante, num_comprobante, fecha_hora, impuesto, total_venta, estado) VALUES ('$idcliente','$idusuario','$tipocomprobante','$seriecomprobante','$numcomprobante','$fechahora','$impuesto','$totalventa','Aceptado')";
         $idventanueva = EjecutarConsultaRetornarID($sql);
 
         $numventas = 0;
@@ -47,7 +47,7 @@ Class CVentas
 
     public function AnularVentas($idventa)
     {
-        $sql = "UPDATE venta SET estado='Anulado' WHERE idventa='$idventa'";
+        $sql = "UPDATE ventas SET estado='Anulado' WHERE idventa='$idventa'";
         return Ejecutar_Consulta($sql);
     }
 
