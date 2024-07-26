b41418ba4952c695ded6d8aa88f606f707f1b2fd
JrnWilliam <antoni.will91@gmail.com>
2024-07-26 09:30:02 -0600

    Se creo el metodo Compras por fecha que nos mostrara las ventas realizadas en un rango de fechas determinado

--- modelos/CConsultas.php
+++ modelos/CConsultas.php
@@ -5,7 +5,13 @@ Class CConsultas
 {
     public function __construct()
     {
-        
+
+    }
+
+    public function Compras_x_Fecha($fechainicio,$fechafin)
+    {
+        $sql = "SELECT DATE(i.fecha_hora) AS fecha,u.nombre AS usuario,p.nombre AS proveedor,i.tipo_comprobante,i.serie_comprobante,i.num_comprobante,i.total_compra,i.impuesto,i.estado FROM ingreso i INNER JOIN persona p ON i.idproveedor = p.idpersona INNER JOIN usuario u ON i.idusuario = u.idusuario WHERE DATE(i.fecha_hora) >= '$fechainicio' AND DATE(i.fecha_hora) <= '$fechafin'";
+        return Ejecutar_Consulta($sql);
     }
 }
 ?>
