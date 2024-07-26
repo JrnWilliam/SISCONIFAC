919ab4e942a19fcc8d63eb8f9d0c9c06499012e7
JrnWilliam <antoni.will91@gmail.com>
2024-07-26 10:27:14 -0600

    Se creo la vista html para hacer consultas por fecha

---
+++ vistas/VConsultaFecha.php
@@ -0,0 +1,27 @@
+<?php
+ob_start();
+session_start();
+
+if(!isset($_SESSION["nombre"]))
+{
+    header("Location: login.html");
+}
+else
+{
+    require 'VHeader.php';
+    if($_SESSION['consultacompras']==1)
+    {
+?>
+
+<?php
+    }
+    else
+    {
+        require 'noacceso.php';
+    }
+    require 'VFooter.php';
+?>
+<?php
+}
+ob_end_flush();  
+?>
