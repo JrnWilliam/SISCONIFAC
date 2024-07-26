72a3ca3be3b6f155917a13db6d45a62edc1448b8
JrnWilliam <antoni.will91@gmail.com>
2024-07-26 10:02:01 -0600

    Se creo codigo con el case filtro por fecha que hara la operacion de realizarla consulta de mostrar los registros por fecha

--- ajax/Consultas.php
+++ ajax/Consultas.php
@@ -3,4 +3,36 @@ require_once '../modelos/CConsultas.php';
 
 $consultas = new CConsultas();
 
+switch($_GET["Operacion"])
+{
+    case 'FiltroXFecha':
+        $finicio = $_REQUEST["f_inicio"];
+        $ffinal = $_REQUEST["f_fin"];
+
+        $respuesta = $consultas->Compras_x_Fecha($finicio,$ffinal);
+
+        $data = Array();
+
+        while($registro = $respuesta->fetch_object())
+        {
+            $data[] = array(
+                "0"=>$registro->fecha,
+                "1"=>$registro->usuario,
+                "2"=>$registro->proveedor,
+                "3"=>$registro->tipo_comprobante,
+                "4"=>$registro->serie_comprobante.'-'.$registro->num_comprobante,
+                "5"=>$registro->total_compra,
+                "6"=>$registro->impuesto,
+                "7"=>($registro->estado == 'Aceptado')?'<span class="label bg-green">Aceptado</span>':'<span class="label bg-red">Cerrado</span>'
+            );
+        }
+        $resultado = array(
+            "sEcho"=>1,
+            "iTotalRecords"=>count($data),
+            "iTotalDisplayRecords"=>count($data),
+            "aaData"=>$data
+        );
+        echo json_encode($resultado); 
+    break;
+}
 ?>
