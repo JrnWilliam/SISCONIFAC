4c461b0367543660f95b83198e70275100106871
JrnWilliam <antoni.will91@gmail.com>
2024-07-26 19:50:20 -0600

    Se agrego el caso Filtro por fecha y cliente que se encargara de mostrar las ventas en rango y por cliente

--- ajax/Consultas.php
+++ ajax/Consultas.php
@@ -34,5 +34,35 @@ switch($_GET["Operacion"])
         );
         echo json_encode($resultado); 
     break;
+    case 'FiltroXFechaCliente':
+        $finicio = $_REQUEST["f_inicio"];
+        $ffinal = $_REQUEST["f_fin"];
+        $id = isset($_REQUEST["id"]) ? $_REQUEST["id"] : "";
+
+        $respuesta = $consultas -> Ventas_x_Fecha_Cliente($finicio,$ffinal,$id);
+
+        $data = Array();
+
+        while($registro = $respuesta->fetch_object())
+        {
+            $data[] = array(
+                "0"=>$registro->fecha,
+                "1"=>$registro->usuario,
+                "3"=>$registro->cliente,
+                "3"=>$registro->tipo_comprobante,
+                "4"=>$registro->serie_comprobante.'-'.$registro->num_comprobante,
+                "5"=>$registro->total_venta,
+                "6"=>$registro->impuesto,
+                "7"=>($registro->estado == 'Aceptado')?'<span class="label bg-green">Aceptado</span>':'<span class="label bg-red">Cerrado</span>'
+            );
+            $resultado = array(
+                "sEcho"=>1,
+                "iTotalRecords"=>count($data),
+                "iTotalDisplayRecords"=>count($data),
+                "aaData"=>$data
+            );
+        }
+        echo json_encode($resultado);
+    break;
 }
 ?>
