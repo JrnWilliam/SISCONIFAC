d6882e6f1dd1c974c318a78f0c69193dc8d56166
JrnWilliam <antoni.will91@gmail.com>
2024-07-26 19:27:40 -0600

    Se agrego el metodo para poder mostrar las ventas realizadas por rango de fecha y por cliente

--- modelos/CConsultas.php
+++ modelos/CConsultas.php
@@ -13,5 +13,11 @@ Class CConsultas
         $sql = "SELECT DATE(i.fecha_hora) AS fecha,u.nombre AS usuario,p.nombre AS proveedor,i.tipo_comprobante,i.serie_comprobante,i.num_comprobante,i.total_compra,i.impuesto,i.estado FROM ingreso i INNER JOIN persona p ON i.idproveedor = p.idpersona INNER JOIN usuario u ON i.idusuario = u.idusuario WHERE DATE(i.fecha_hora) >= '$fechainicio' AND DATE(i.fecha_hora) <= '$fechafin'";
         return Ejecutar_Consulta($sql);
     }
+
+    public function Ventas_x_Fecha_Cliente($fechainicio,$fechafin,$idcliente)
+    {
+        $sql = "SELECT DATE(v.fecha_hora) AS fecha,u.nombre AS usuario,p.nombre as cliente,v.tipo_comprobante,v.serie_comprobante,v.num_comprobante,v.total_venta,v.impuesto,v.estado FROM ventas v INNER JOIN persona p ON v.idcliente = p.idpersona INNER JOIN usuario u ON v.idusuario = u.idusuario WHERE DATE(v.fecha_hora) >= '$fechainicio' AND DATE(v.fecha_hora) <= '$fechafin' AND v.idcliente='$idcliente'";
+        return Ejecutar_Consulta($sql);
+    }
 }
 ?>
