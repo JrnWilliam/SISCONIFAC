9ba98871aebddc6e4944ecf48f01b4c7250beb4f
JrnWilliam <antoni.will91@gmail.com>
2024-07-29 11:17:01 -0600

    Se creo el archivo para poder crear un reporte en formato pdf

---
+++ files/reportes/RArticulo.php
@@ -0,0 +1,23 @@
+<?php
+ob_start();
+session_start();
+
+if(!isset($_SESSION['nombre']))
+{
+    echo "Ingrese al Sistema Para Visualizar Este Reporte";
+}
+else
+{
+    if($_SESSION['almacen']==1)
+    {
+?>
+
+<?php
+    }
+    else
+    {
+        echo "No Tiene Permisos Para Visualizar Este Reporte";
+    }
+}
+ob_end_flush();  
+?>
