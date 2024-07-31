fa357572409dd46ca1c415962fd49b6fcb7cd4a4
JrnWilliam <antoni.will91@gmail.com>
2024-07-31 10:50:34 -0600

    Se creo el codigo con el cual se manejara la cabecera de la factura

--- reportes/FIngreso.php
+++ reportes/FIngreso.php
@@ -11,6 +11,47 @@
         if($_SESSION['compras']==1)
         {
             require 'MFactura.php';
+
+            $logo = "Logo.jpg";
+            $extension = "jpg";
+            $empresa = "Ferreteria Mi Bendición S.A";
+            $ruc = "J0310000002118";
+            $direccion = "León, Nicaragua";
+            $telefono = "+505 2222-2222";
+            $email = "fmibendicion@ejemplo.com";
+
+            require_once '../modelos/CIngresos.php';
+            $Objingreso = new CIngresos();
+
+            $respuestai = $Objingreso->CabeceraFacIngreso($_GET["id"]);
+            $registroi = $respuestai->fetch_object();
+
+            $pdf = new PDF_Invoice('P','mm','A4');
+            $pdf->AddPage();
+
+            $pdf -> addSociete(
+                utf8_decode($empresa),
+                $ruc."\n".
+                utf8_decode("Dirección: ").utf8_decode($direccion)."\n".
+                utf8_decode("Teléfono: ").$telefono."\n".
+                "email: ".$email,
+                $logo,
+                $extension
+            );
+
+            $pdf->fact_dev(
+                "$registroi->tipo_comprobante"." ",
+                "$registroi->serie_comprobante",
+                "$registroi->num_comprobante"
+            );
+
+            $pdf->addClientAdresse(
+                utf8_decode($registroi->proveedor),
+                utf8_decode("Direccion: ").utf8_decode($registroi->direccion),
+                utf8_decode($registroi->tipo_documento).": ".$registroi->num_documento,
+                "Email: ".$registroi->email,
+                "Telefono: ".$registroi->telefono
+            );
         }
         else
         {
