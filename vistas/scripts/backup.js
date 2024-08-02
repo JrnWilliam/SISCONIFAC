05928ae69e8ba64cea17374338aca307170e3da6
JrnWilliam <antoni.will91@gmail.com>
2024-08-01 19:49:18 -0600

    Se creo el codigo para capturar el evento cuando se haga clic sobre el enlace que genera el backup de la BD

---
+++ vistas/scripts/backup.js
@@ -0,0 +1,26 @@
+function IniciarBackUp(event)
+{
+    event.preventDefault();
+    RealizarBackUp()
+}
+
+function RealizarBackUp()
+{
+    
+    $.ajax(
+        {
+            url: '../ajax/BackUp.php',
+            type: 'GET',
+            success: function(response)
+            {
+                bootbox.alert("Se Realizo Correctamente el BackUp de la BD")
+            },
+            error: function(xhr, status, error)
+            {
+                bootbox.alert("Error Al Realizar El BackUp: " + error)
+            }
+        }
+    )
+}
+
+IniciarBackUp()
