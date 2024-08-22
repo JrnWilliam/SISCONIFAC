98a9cc03939b5f75d722c8296f567fead11cae29
JrnWilliam <antoni.will91@gmail.com>
2024-08-21 22:09:55 -0600

    Se agrego el codigo para verificar la subida del archivo, que se invoque a la funcion restauracion, y que controle los errores

--- ajax/Restaurar.php
+++ ajax/Restaurar.php
@@ -1,3 +1,22 @@
 <?php
+require "../config/config.php";
+include('../modelos/CRestaurar.php');
+
+if(isset($_FILES['script']))
+{
+    $route = $_FILES['script']['tmp_name'];
     
+    if(RestaurarDB(DBHOST, DB_USERNAME, DB_PASSWORD,$route))
+    {
+        echo "Restauración Completada con Éxito.";
+    }
+    else
+    {
+        echo "Error Durante la Restauración.";
+    }
+}
+else
+{
+    echo "No ha Subido Ningún Archivo.";
+}
 ?>
